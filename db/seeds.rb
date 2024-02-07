66eba409760d0dc86207f132d4fd1136aa7accb3
Liene <Liene@LAPTOP-C1G7S8GH>
2024-02-07 20:26:29 +0200

    devise added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email: 'liene@example.com', password: 'password', password_confirmation: 'password')
+
+10.times do |x|
+  Post.create(title: "Title #{x}", body: "Body #{x} words go here", user_id: User.first.id)
+end
